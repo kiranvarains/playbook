6244d8547a761f232728caf219d72571606b875f
kumanvarains <kumarvarains7@gmail.com>
2022-04-20 19:36:13 +0530

    update

---
+++ precks.sh
@@ -0,0 +1,38 @@
+echo ##########mounted files -info###############################3
+df -h
+#
+echo ========date========
+date
+#
+echo ========uptime========
+uptime
+#
+echo =======who=====
+who
+#
+echo=======uname -r======
+uname -r
+#
+echo =======whoami=====
+whoami
+#
+echo ======who am i ======
+who am i
+#
+echo =====pwd=======
+pwd
+#
+echo ====free -g=====
+free -g
+#
+echo ====pwd======
+pwd
+#
+echo ======netstat -nr========
+netstat -nr
+#
+echo =======Swap info=========
+swapon -s
+#
+echo =======ram and swap info ========
+free -h
